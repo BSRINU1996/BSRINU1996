e4f429b6fc88123a3127d91f45caae0ce201dcd4
MeSrinu <balisrinu1996@gmail.com>
2023-09-12 00:20:57 +0530

    changed the gd link in handleClick.js

--- src/Components/handleclick.js
+++ src/Components/handleclick.js
@@ -1,5 +1,5 @@
 function handleclick() {
-  window.open("https://drive.google.com/file/d/1JBqksFjJlZQMolre6C8E38w7-8kvWLUs/view?usp=drive_link")
+  window.open("https://drive.google.com/file/d/1h4TQnXFjEoOUFA0ze7ABGcToROnclpJ0/view?usp=drive_link")
   window.location.href = 'https://drive.google.com/uc?export=download&id=1F3e97lvsMWeixkldYG4YlnEmZDiU6COC'
 }
 export default handleclick
