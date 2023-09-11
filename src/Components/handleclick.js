7a2f3a9cfcf0004b290f46313514ac059c870df0
MeSrinu <balisrinu1996@gmail.com>
2023-09-11 23:00:26 +0530

    portifolio first code commit

---
+++ src/Components/handleclick.js
@@ -0,0 +1,9 @@
+function handleclick() {
+  window.open("https://drive.google.com/file/d/1JBqksFjJlZQMolre6C8E38w7-8kvWLUs/view?usp=drive_link")
+  window.location.href = 'https://drive.google.com/uc?export=download&id=1F3e97lvsMWeixkldYG4YlnEmZDiU6COC'
+}
+export default handleclick
+
+// https://drive.google.com/file/d/1F3e97lvsMWeixkldYG4YlnEmZDiU6COC/view?usp=drive_link
+//https://drive.google.com/file/d/1F3e97lvsMWeixkldYG4YlnEmZDiU6COC/view?usp=drive_link
+//https://drive.google.com/file/d/1F3e97lvsMWeixkldYG4YlnEmZDiU6COC/view?usp=sharing
