f7459cf26b9060ead76322eb28ffae2e1cd03863
MeSrinu <balisrinu1996@gmail.com>
2023-09-12 11:48:34 +0530

    changed resume

--- src/Components/handleclick.js
+++ src/Components/handleclick.js
@@ -1,5 +1,5 @@
 function handleclick() {
-  window.open("https://drive.google.com/file/d/1h4TQnXFjEoOUFA0ze7ABGcToROnclpJ0/view?usp=drive_link")
+  window.open("https://drive.google.com/file/d/1a9qiwFJpkYb5OgX01dJpqfAPlMyLA1VG/view?usp=drive_link")
   window.location.href = 'https://drive.google.com/uc?export=download&id=1F3e97lvsMWeixkldYG4YlnEmZDiU6COC'
 }
 export default handleclick
